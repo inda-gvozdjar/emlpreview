18a432bbb9c2af8b0cd0b571fe255381fd6dde65
Inda <inda.gvozdjar@gmail.com>
2022-11-08 17:07:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {simpleParser} from 'mailparser'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      fileName: '',
+      fileContent: ''
+    }
+  }
+  handleFileChange = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    
+    reader.onload = () => {
+      
+      this.setState({fileName: file.name, fileContent: reader.result})
+      console.log(reader.result, "oooooo")
+      console.log(reader, "eeeeee")
+      console.log(file, "filee")
+    }
+    reader.onerror = () => {
+      console.log(reader.error, "file error")
+    }}
+  
+  render() {
+    
+    return(
+      <div>
+        <h1>file reader</h1>
+        <input type = "file" onChange={this.handleFileChange}></input>
+        <br/>
+        <p>{this.state.fileName}</p>
+        <p>content in html  {this.state.fileContent}</p>
+        <div dangerouslySetInnerHTML={{ __html: this.state.fileContent }} />
+      </div>
+    )
+}}
+export default App
